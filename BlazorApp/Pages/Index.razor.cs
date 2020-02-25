e6fdea66f6a6bda18c41b96bcdf90bf18931128d
강상훈 <ishkang@gmail.com>
2020-02-25 23:13:04 +0900

    초기 커밋.

---
+++ BlazorApp/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using Grpc.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class Index
+    {
+        private string InitialMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Test.TestClient(channel);
+            var reply = await client.DoAsync(
+                              new Request { Message = this.ToString() });
+
+            InitialMessage = reply.Message;
+        }
+    }
+}
---
+++ GrpcService.Server/Services/TestService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Service;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcService.Server
+{
+    public class TestService : Test.TestBase
+    {
+        //public TestService(ILogger logger)
+        //{
+        //    this.Loger = logger;
+        //}
+
+        //public ILogger Loger { get; }
+
+        public override Task<Response> Do(Request request, ServerCallContext context)
+        {
+            IDictionary<string, object> responseData = new Dictionary<string, object>
+            {
+                ["request"] = request.Message
+            };
+
+            return Task.FromResult<Response>(new Response()
+            {
+                Message = JsonConvert.SerializeObject(responseData)
+            });
+        }
+    }
+}
